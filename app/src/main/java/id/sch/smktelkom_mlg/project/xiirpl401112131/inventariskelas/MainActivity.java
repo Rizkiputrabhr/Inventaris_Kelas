6a9e933fd2f1c16049e73de8580c0130ccbaf818
achaslan <achmadnuurfadhillah@gmail.com>
2016-11-20 11:58:19 +0700

    UPDATE halaman Login

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl401112131/inventariskelas/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl401112131/inventariskelas/MainActivity.java
@@ -1,12 +1,17 @@
 package id.sch.smktelkom_mlg.project.xiirpl401112131.inventariskelas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mloginBtn;
 
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +37,31 @@ public class MainActivity extends AppCompatActivity {
 
         mloginBtn = (Button) findViewById(R.id.loginBtn);
 
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if (firebaseAuth.getCurrentUser() == null) {
+
+                    startActivity(new Intent(MainActivity.this, Home.class));
+                }
+            }
+        };
+
 
         mloginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-                                         @Override
-                                         public void OnClick(View view) {
+            }
+        });
 
-                                             startSignIn();
-                                         }
+    }
 
-                                     }
-        );
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
     private void startSignIn() {
@@ -50,12 +69,26 @@ public class MainActivity extends AppCompatActivity {
         String email = memailField.getText().toString();
         String password = mpasswordField.getText().toString();
 
-        mAuth.signInWithEmailAndPassword(email, password.addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
 
-                                                                               }
+            Toast.makeText(MainActivity.this, "Email atau Password Kosong! ", Toast.LENGTH_LONG).show();
+
+        } else {
+
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (!task.isSuccessful()) {
+
+                        Toast.makeText(MainActivity.this, "Email atau Pasword Salah! ", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
 
 
-        ))
+
 
     }
 }
